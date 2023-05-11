03bf4a1f48dc4638d2ecf0951d7759a75d2b0c1c
Wayne Huang <wayne123tw@gmail.com>
2023-05-11 23:00:56 +0800

    [FEATURE] add github action

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  collectCoverage: true,
   coverageDirectory: "coverage",
   preset: "ts-jest",
   testEnvironment: "node",
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  coverageReporters: ["text", "cobertura"],
 };
